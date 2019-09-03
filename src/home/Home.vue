44b024109c1dd7f80777867e90afc5c857c4db4b
matvey <msmych@yandex.com>
2019-09-03 22:51:17 +0300

    Getting user totals

--- src/home/Home.vue
+++ src/home/Home.vue
@@ -1,17 +1,28 @@
 <template>
   <div>
-    <h1>API token: {{ apiToken }}</h1>
+    <h1>{{ userTotals }}</h1>
   </div>
 </template>
 
 <script>
   import {mapGetters} from 'vuex'
+  import axios from 'axios';
 
   export default {
     computed: {
       ...mapGetters({
-        apiToken: 'apiToken'
+        apiToken: 'apiToken',
+        userId: 'userId',
+        userTotals: 'userTotals'
       })
+    },
+    mounted() {
+      axios.get(`/total/in/${this.userId}/type/MONTH`, {
+        headers: {
+          'user-id': this.userId,
+          'api-token': this.apiToken
+        }
+      }).then(totals => this.$store.commit('setUserTotal', totals.data))
     }
   }
 </script>
--- src/store.js
+++ src/store.js
@@ -6,17 +6,37 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
   state: {
     apiToken: null,
-    user: {}
+    user: {
+      id: "",
+      totals: {}
+    }
   },
   getters: {
     apiToken(state) {
       return state.apiToken;
+    },
+    userId(state) {
+      return state.user.id;
+    },
+    userTotals(state) {
+      return state.user.totals;
     }
   },
   mutations: {
     setApiToken(state, payload) {
       state.apiToken = payload.apiToken;
-      state.user.id = payload.userId
+      state.user.id = payload.userId;
+    },
+    setUserTotal(state, totals) {
+      const userTotals = {};
+      totals.forEach(total => {
+        if (userTotals[total.currency]) {
+          userTotals[total.currency].push(total.amount);
+        } else {
+          userTotals[total.currency] = [total.amount];
+        }
+      });
+      state.user.totals = userTotals;
     }
   }
 });
