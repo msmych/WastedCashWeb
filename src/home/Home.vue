2ea255f6d02eb53906eeeda1af11b6c8d5e4c83b
Matvey Smychkov <matvey.smychkov@revolut.com>
2019-09-04 21:44:40 +0300

    Added total type

--- src/home/Home.vue
+++ src/home/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <h3>My total</h3>
+    <input type="radio" id="month" value="MONTH" v-model="type">
+    <label for="month">Month</label>
+    <input type="radio" id="all" value="ALL" v-model="type">
+    <label for="month">All</label>
     <div v-for="userTotal in userTotals">
       <h3>{{ userTotal.amount }} {{ userTotal.currency }}</h3>
     </div>
@@ -11,6 +16,11 @@
   import axios from 'axios';
 
   export default {
+    data() {
+      return {
+        type: "MONTH"
+      }
+    },
     computed: {
       ...mapGetters({
         apiToken: 'apiToken',
@@ -19,7 +29,7 @@
       })
     },
     mounted() {
-      axios.get(`/total/in/${this.userId}/type/MONTH`, {
+      axios.get(`/total/in/${this.userId}/type/${this.type}`, {
         headers: {
           'user-id': this.userId,
           'api-token': this.apiToken
